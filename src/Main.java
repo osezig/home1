58215788742875b3802edf3dbb2389ea62c1b321
sergey_iagfarov <dedaz27@mail.ru>
2022-09-22 23:56:10 +0500

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1");
+        double dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+        // Задача 2
+        System.out.println("Задача 2");
+        dog += 4;
+        System.out.println(dog);
+        cat += 4;
+        System.out.println(cat);
+        paper += 4;
+        System.out.println(paper);
+
+        // Задача 3
+        System.out.println("Задача 3");
+        dog -= 3.5;
+        System.out.println(dog);
+        cat -= 1.6;
+        System.out.println(cat);
+        paper -= 7639;
+        System.out.println(paper);
+    }
+}
